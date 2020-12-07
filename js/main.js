6f187100840645a61770633f604916d836a09991
lampa-lana <lampa1979@mail.ru>
2020-12-07 22:15:17 +0300

    create: added new img., keybord ctrl for slaider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-
+  keyboard: {
+  enabled: true,
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button_next',
     prevEl: '.slider-button_prev',
   },
-  effect: "cube"
+  effect: "fade",
 })
