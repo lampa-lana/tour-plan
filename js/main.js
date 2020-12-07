2cb5fb1b3c4e9e8b02d9ab3b07016c8d493c63a7
lampa-lana <lampa1979@mail.ru>
2020-12-07 21:54:29 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
+  effect: "cube"
+})
